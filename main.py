4c6fa0d7e863790528b84a67da3e0e61f62ef5fe
Miguel Lopez <ing.miguell@gmail.com>
2023-04-10 17:56:09 -0600

    First commit and create the screen

---
+++ main.py
@@ -0,0 +1,10 @@
+from turtle import Screen
+
+
+screen = Screen()
+screen.tracer()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("PONG, The game!")
+
+screen.exitonclick()
